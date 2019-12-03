d90cacbf597e6d0fce7efeb2128cbf23ac280e42
Min-32-kyp <mi.hayashi30@gmail.com>
2019-12-03 23:56:34 +0900

    make html

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,45 @@
 $(function(){
+  function buildHTML(message){
+    // 「もしメッセージに画像が含まれていたら」という条件式
+    if (message.image) {
+      var html = //メッセージに画像が含まれる場合のHTMLを作る
+      `<div class="message">
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.date}
+          </div>
+        </div>
+        <div class="lower-meesage">
+          <p class="lower-message__content">
+            ${message.body}
+          </p>
+        </div>
+        <asset_path src=${message.image} >
+      </div>`
+    } else {
+      var html = //メッセージに画像が含まれない場合のHTMLを作る
+      `<div class="message">
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+          <div class="upper-message__date">
+            ${message.date}
+          </div>
+        </div>
+        <div class="lower-meesage">
+          <p class="lower-message__content">
+          ${message.body}
+          </p>
+        </div>
+      </div>`
+    }
+    return html
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +52,14 @@ $(function(){
       processData: false,
       contentType: false
     })
+
+    .done(function(data) {
+       var html = buildHTML(data);
+      $('.main-content__chat-contents').append(html)
+      $('#message_body').val('')
+    })
+    .fail(function() {
+      alert('error');
+    });
   })
 });
